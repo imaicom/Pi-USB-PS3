094aac7879f55afd2f83d08c2a0d323dc95a62c8
imaicom <i@okako.com>
2016-05-03 21:30:00 +0900

    Update controller.c

--- controller.c
+++ controller.c
@@ -94,7 +94,7 @@ int ps3c_init(struct ps3ctls *ps3dat, const char *df) {
 		return -2;
 	}
 
-	p = calloc((nr_btn + nr_stk) * sizeof(short));
+	p = calloc(1 , (nr_btn + nr_stk) * sizeof(short));
 	if (p == NULL) {
 		close(ps3dat->fd);
 		return -3;
